37167db5327444b658ba5d116ff27d770c65aa5c
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2016-10-03 10:18:09 +0300

    Coverity issue fix.
    Adjusted Adaptation::Icap::ModXact::finalizeLogInfo(), fixing possible
    "Null pointer dereference".

--- src/adaptation/icap/ModXact.cc
+++ src/adaptation/icap/ModXact.cc
@@ -1261,7 +1261,7 @@ void Adaptation::Icap::ModXact::finalizeLogInfo()
 {
     HttpRequest *adapted_request_ = nullptr;
     HttpReply *adapted_reply_ = nullptr;
-    HttpRequest *virgin_request_ = (virgin.cause ? virgin.cause : dynamic_cast<HttpRequest*>(virgin.header));
+    HttpRequest *virgin_request_ = const_cast<HttpRequest*>(&virginRequest());
     if (!(adapted_request_ = dynamic_cast<HttpRequest*>(adapted.header))) {
         // if the request was not adapted, use virgin request to simplify
         // the code further below
