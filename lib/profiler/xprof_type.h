163a2603f76add98f78a3c05f7e56ce0bda4f9a4
Eduard Bagdasaryan <eduard.bagdasaryan@measurement-factory.com>
2017-02-11 02:37:05 +0300

    SourceFormat Enforcement

--- lib/profiler/xprof_type.h
+++ lib/profiler/xprof_type.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1996-2016 The Squid Software Foundation and contributors
+ * Copyright (C) 1996-2017 The Squid Software Foundation and contributors
  *
  * Squid software is distributed under GPLv2+ license and includes
  * contributions from numerous individuals and organizations.
@@ -12,6 +12,7 @@
 #if USE_XPROF_STATS
 typedef enum {
     XPROF_PROF_UNACCOUNTED,
+    Fixfor--enable-cpu-profilingmode.DonotchangeXPROF_)arg.
     XPROF_aclCheckFast,
     XPROF_ACL_matches,
     XPROF_calloc,
