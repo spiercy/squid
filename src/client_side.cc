b3a4fcaaeeb2df51b84cd899dec21f45944b684a
Alex Rousskov <rousskov@measurement-factory.com>
2018-04-25 16:12:22 -0600

    Optimization: Do not create/configure ACLFilledChecklist in vain
    While client_db is required for client-side pools to work, it may be
    enabled for other reasons, without any client-side pools configured. We
    should not create and configure useless ACLFilledChecklist objects
    because those operations are already not trivial today and have a
    tendency of becoming more expensive with time.

--- src/client_side.cc
+++ src/client_side.cc
@@ -2486,12 +2486,13 @@ ConnStateData::start()
 #if USE_DELAY_POOLS
     fd_table[clientConnection->fd].clientInfo = NULL;
 
-    if (Config.onoff.client_db) {
-        /* it was said several times that client write limiter does not work if client_db is disabled */
+    if (!Config.onoff.client_db)
+        return; // client delay pools require client_db
 
-        auto &pools = ClientDelayPools::Instance()->pools;
+    auto &pools = ClientDelayPools::Instance()->pools;
+    if (pools.size()) {
         ACLFilledChecklist ch(NULL, NULL, NULL);
- 
+
         // TODO: we check early to limit error response bandwith but we
         // should recheck when we can honor delay_pool_uses_indirect
         // TODO: we should also pass the port details for myportname here.
