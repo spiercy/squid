9d95cbbf1c987295b7d8a529bce0d37d5d733571
Alex Rousskov <rousskov@measurement-factory.com>
2018-06-04 16:34:28 -0600

    Add al->request locking missing from the previous branch commit

--- src/servers/FtpServer.cc
+++ src/servers/FtpServer.cc
@@ -755,7 +755,10 @@ Ftp::Server::parseOneRequest()
     HTTPMSGLOCK(http->request);
     http->req_sz = tok.parsedSize();
     http->uri = newUri;
-    http->al->request = request;
+    if (!http->al->request) { // should be nil -- ClientHttpRequest() creates al
+        http->al->request = request;
+        HTTPMSGLOCK(http->al->request);
+    }
 
     Http::Stream *const result =
         new Http::Stream(clientConnection, http);
