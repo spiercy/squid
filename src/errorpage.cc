8a4a5a58b108abceef1bfde3339ca59c9a42af11
Christos Tsantilas <christos@chtsanti.net>
2018-07-06 12:49:40 +0300

    Remove unneeded changes

--- src/errorpage.cc
+++ src/errorpage.cc
@@ -561,25 +561,25 @@ ErrorState::NewForwarding(err_type type, HttpRequest *request)
 ErrorState::ErrorState(err_type t) :
     type(t),
     page_id(t),
-    err_language(nullptr),
+    err_language(NULL),
     httpStatus(Http::scNone),
 #if USE_AUTH
-    auth_user_request(nullptr),
+    auth_user_request (NULL),
 #endif
-    request(nullptr),
-    url(nullptr),
+    request(NULL),
+    url(NULL),
     xerrno(0),
     port(0),
     dnsError(),
     ttl(0),
     src_addr(),
-    redirect_url(nullptr),
-    callback(nullptr),
-    callback_data(nullptr),
-    request_hdrs(nullptr),
-    err_msg(nullptr),
+    redirect_url(NULL),
+    callback(NULL),
+    callback_data(NULL),
+    request_hdrs(NULL),
+    err_msg(NULL),
 #if USE_OPENSSL
-    detail(nullptr),
+    detail(NULL),
 #endif
     detailCode(ERR_DETAIL_NONE)
 {
