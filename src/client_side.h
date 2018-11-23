91c378e014fde05ebc014199bd004c748925d326
Christos Tsantilas <christos@chtsanti.net>
2018-11-23 17:59:18 +0200

    Fix on_unsupported_protocol issues
    The on_unsupported_protocol will not work in the following cases:
    - Bump with client-first or bump on step1 will not work because
      ConnStateData::mayTunnelUnsupportedProto() fails because there is not any
      pinned connection
    - splice  unknown protocol inside client-first bumped connection does not
      work because ConnStateData::initiateTunneledRequest can not compute a URL,
      because there is not any pinned connection and cause (HttpRequest) is
      missing.
    - Squid in many cases buffers the client request even if not really needed,
      for example on all HTTP requests passed through a bumped connection
      because of ConnStateData::mayTunnelUnsupportedProto();
    
    This patch also unables the use of on_unsupported_protocol on plain http ports
    in transparent proxying mode.

--- src/client_side.h
+++ src/client_side.h
@@ -397,6 +397,7 @@ private:
 
     /// The SSL server host name appears in CONNECT request or the server ip address for the intercepted requests
     String sslConnectHostOrIp; ///< The SSL server host name as passed in the CONNECT request
+    int tlsConnectPort; ///< The TLS server port used in the CONNECT request
     SBuf sslCommonName_; ///< CN name for SSL certificate generation
 
     /// TLS client delivered SNI value. Empty string if none has been received.
