d85a67980cfa8bc1e6af6ab6568e71c3137a30b1
Alex Rousskov <rousskov@measurement-factory.com>
2019-03-04 12:25:20 -0700

    Call syncHierNote() in tunnel.cc failure cases, like FwdState does
    I am not 100% sure whether syncHierNote() is needed in tunnel.cc (and,
    in fact, in FwdState), but it is probably best to keep this forwarding
    _consistent_ across these two objects by default.
    
    N.B. Neither FwdState nor tunnel.cc can recover from usePinned()
    failures AFAICT: Neither useDestinations() and eventually reach
    noteConnection() after usePinned() detects a closed connection. Both
    declare a terminal failure instead. This may imply that we do not need
    to call syncHierNote() in both pinned and HappyConnOpener failure cases,
    in both tunnel.cc and FwdState code, but I am not sure. At least the
    callers are consistent/similar now.

--- src/tunnel.cc
+++ src/tunnel.cc
@@ -195,6 +195,8 @@ public:
     virtual void noteDestination(Comm::ConnectionPointer conn) override;
     virtual void noteDestinationsEnd(ErrorState *selectionError) override;
 
+    void syncHierNote(const Comm::ConnectionPointer &server, const char *origin);
+
     /// called when a connection has been successfully established or
     /// when all candidate destinations have been tried and all have failed
     void noteConnection(HappyConnOpenerAnswer &);
@@ -340,6 +342,7 @@ TunnelStateData::TunnelStateData(ClientHttpRequest *clientRequest) :
     assert(clientRequest);
     url = xstrdup(clientRequest->uri);
     request = clientRequest->request;
+    Must(request);
     server.size_ptr = &clientRequest->out.size;
     client.size_ptr = &clientRequest->al->http.clientRequestSz.payloadData;
     status_ptr = &clientRequest->al->http.code;
@@ -396,6 +399,16 @@ TunnelStateData::Connection::bytesIn(int const &count)
 #endif
 
     len += count;
+}
+
+/// update "hierarchy" annotations with a new (possibly failed) destination
+/// \param origin the name of the origin server we were trying to reach
+void
+TunnelStateData::syncHierNote(const Comm::ConnectionPointer &conn, const char *origin)
+{
+    request->hier.resetPeerNotes(conn, origin);
+    if (al)
+        al->hier.resetPeerNotes(conn, origin);
 }
 
 int
@@ -1034,6 +1047,7 @@ TunnelStateData::noteConnection(HappyConnOpener::Answer &answer)
     connOpener.clear();
 
     if (const auto err = answer.error.get()) {
+        syncHierNote(answer.conn, request->url.host());
         saveError(err);
         answer.error.clear(); // savedError has it now
         sendError(savedError, "tried all destinations");
@@ -1052,6 +1066,8 @@ TunnelStateData::connectDone(const Comm::ConnectionPointer &conn, const char *or
     if (reused)
         ResetMarkingsToServer(request.getRaw(), *conn);
     // else Comm::ConnOpener already applied proper/current markings
+
+    syncHierNote(server.conn, request->url.host());
 
     request->hier.resetPeerNotes(conn, origin);
     if (al)
@@ -1352,17 +1368,18 @@ TunnelStateData::startConnecting()
 void
 TunnelStateData::usePinned()
 {
+    Must(request);
+    const auto connManager = request->pinnedConnection();
     auto serverConn = borrowPinnedConnection(request.getRaw());
     debugs(26,7, "pinned peer connection: " << serverConn);
     if (!Comm::IsConnOpen(serverConn)) {
+        syncHierNote(serverConn, connManager ? connManager->pinning.host : request->url.host());
         // a PINNED path failure is fatal; do not wait for more paths
         sendError(new ErrorState(ERR_CANNOT_FORWARD, Http::scServiceUnavailable, request.getRaw()),
                   "pinned path failure");
         return;
     }
 
-    Must(request);
-    const auto connManager = request->pinnedConnection();
     Must(connManager);
 
     // Set HttpRequest pinned related flags for consistency even if
